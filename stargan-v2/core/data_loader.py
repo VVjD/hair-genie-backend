210c1ad2489959e65567076870e6bbddee22a4f2
chaeriIM <tldzmvpzl@naver.com>
2023-11-28 15:48:04 +0900

    StarGAN v2 코드 수정2

--- stargan-v2/core/data_loader.py
+++ stargan-v2/core/data_loader.py
@@ -10,11 +10,16 @@ Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 
 from pathlib import Path
 from itertools import chain
+import os
+import random
 
 from munch import Munch
+from PIL import Image
+import numpy as np
 
 import torch
 from torch.utils import data
+from torch.utils.data.sampler import WeightedRandomSampler
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 
@@ -24,6 +29,60 @@ def listdir(dname):
                           for ext in ['png', 'jpg', 'jpeg', 'JPG']]))
     return fnames
 
+
+class DefaultDataset(data.Dataset):
+    def __init__(self, root, transform=None):
+        self.samples = listdir(root)
+        self.samples.sort()
+        self.transform = transform
+        self.targets = None
+
+    def __getitem__(self, index):
+        fname = self.samples[index]
+        img = Image.open(fname).convert('RGB')
+        if self.transform is not None:
+            img = self.transform(img)
+        return img
+
+    def __len__(self):
+        return len(self.samples)
+
+
+class ReferenceDataset(data.Dataset):
+    def __init__(self, root, transform=None):
+        self.samples, self.targets = self._make_dataset(root)
+        self.transform = transform
+
+    def _make_dataset(self, root):
+        domains = os.listdir(root)
+        fnames, fnames2, labels = [], [], []
+        for idx, domain in enumerate(sorted(domains)):
+            class_dir = os.path.join(root, domain)
+            cls_fnames = listdir(class_dir)
+            fnames += cls_fnames
+            fnames2 += random.sample(cls_fnames, len(cls_fnames))
+            labels += [idx] * len(cls_fnames)
+        return list(zip(fnames, fnames2)), labels
+
+    def __getitem__(self, index):
+        fname, fname2 = self.samples[index]
+        label = self.targets[index]
+        img = Image.open(fname).convert('RGB')
+        img2 = Image.open(fname2).convert('RGB')
+        if self.transform is not None:
+            img = self.transform(img)
+            img2 = self.transform(img2)
+        return img, img2, label
+
+    def __len__(self):
+        return len(self.targets)
+
+
+def _make_balanced_sampler(labels):
+    class_counts = np.bincount(labels)
+    class_weights = 1. / class_counts
+    weights = class_weights[labels]
+    return WeightedRandomSampler(weights, len(weights))
 
 def get_test_loader(root, img_size=256, batch_size=32,
                     shuffle=True, num_workers=4):
--- stargan-v2/core/utils.py
+++ stargan-v2/core/utils.py
@@ -8,6 +8,8 @@ http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
 Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 """
 
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torchvision.utils as vutils
 
@@ -21,7 +23,34 @@ def save_image(x, ncol, filename):
     x = denormalize(x)
     vutils.save_image(x.cpu(), filename, nrow=ncol, padding=0)
 
+@torch.no_grad()
+def translate_using_reference(nets, args, x_src, x_ref, y_ref, filename):
+    N, C, H, W = x_src.size()
+    wb = torch.ones(1, C, H, W).to(x_src.device)
+    x_src_with_wb = torch.cat([wb, x_src], dim=0)
 
-def tensor2ndarray255(images):
-    images = torch.clamp(images * 0.5 + 0.5, 0, 1)
-    return images.cpu().numpy().transpose(0, 2, 3, 1) * 255
+    masks = nets.fan.get_heatmap(x_src) if args.w_hpf > 0 else None
+    s_ref = nets.style_encoder(x_ref, y_ref)
+    s_ref_list = s_ref.unsqueeze(1).repeat(1, N, 1)
+    x_concat = [x_src_with_wb]
+    for i, s_ref in enumerate(s_ref_list):
+        x_fake = nets.generator(x_src, s_ref, masks=masks)
+        x_fake_with_ref = torch.cat([x_ref[i:i+1], x_fake], dim=0)
+        x_concat += [x_fake_with_ref]
+
+    img__ = tr_image(x_fake)
+    plt.imsave(args.result_dir+'/out.png', img__)
+
+    x_concat = torch.cat(x_concat, dim=0)
+    save_image(x_concat, N+1, filename)
+    del x_concat
+
+# 이미지 변환
+def tr_image(img):
+    img_ = img.squeeze()
+    img_ = img_.detach().cpu().numpy()
+    img_ = np.transpose(img_, (1, 2, 0))
+    mean_, std_ = np.array([0.5, 0.5, 0.5]), np.array([0.5, 0.5, 0.5])
+    img_ = np.clip(255.0 * (img_ * std_ + mean_), 0, 255)
+    img_ = img_.astype(np.uint8)
+    return img_
