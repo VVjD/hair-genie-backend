de35d96d02c539c540f1e291b3c5f4049aff1d9d
VVjD <hj828@kakao.com>
2023-12-06 21:42:47 +0900

    카테고리 추가

--- board/admin.py
+++ board/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Board
 
 class BoardAdmin(admin.ModelAdmin):
-    list_display = ('customer', 'title', 'content', 'views_count', 'created_at')
-    list_filter = ('customer',)
+    list_display = ('customer', 'category', 'title', 'content', 'views_count', 'created_at')
+    list_filter = ('customer','category')
 
 admin.site.register(Board, BoardAdmin)
--- board/urls.py
+++ board/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.BoardListCreateView.as_view(), name='board'),
     path('<int:pk>/', views.BoardRetrieveUpdateDestroyView.as_view(), name='board-detail'),
+    path('<int:pk>/increment-views/', views.IncrementViews.as_view(), name='increment-views'),
 ]
